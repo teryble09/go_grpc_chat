931b017fef63d747af41e05485fdb2b2eb9baf61
Selyushkin Dmitry <teryble09@yandex.ru>
2025-06-08 14:08:55 +0300

    finished logs

--- server/server.go
+++ server/server.go
@@ -28,7 +28,7 @@ func (srv *GrpcServer) Login(ctx context.Context, logReq *proto.LoginRequest) (*
 	err = srv.Db.RegisterUser(ctx, user)
 	if err != nil {
 		if err == custom_errors.ErrUserAlreadyExist {
-			srv.Logger.Info("Trying to register already existing user", "username", user.Username)
+			srv.Logger.Info("Trying to register already existing", "username", user.Username)
 			return nil, status.Error(codes.AlreadyExists, "user with this name already exists")
 		} else {
 			srv.Logger.Warn("Error in db.RegisterUser", "error", err.Error())
@@ -36,7 +36,7 @@ func (srv *GrpcServer) Login(ctx context.Context, logReq *proto.LoginRequest) (*
 		}
 	}
 
-	srv.Logger.Info("Registered user", "user", user.Username)
+	srv.Logger.Info("Registered", "user", user.Username)
 	return &proto.LoginResponse{}, nil
 }
 
@@ -44,10 +44,12 @@ func (srv *GrpcServer) Stream(cnn proto.Chat_StreamServer) error {
 	ctx := cnn.Context()
 	md, ok := metadata.FromIncomingContext(ctx)
 	if !ok {
+		srv.Logger.Warn("could not load metadata")
 		return status.Error(codes.Unavailable, "can not load metadata")
 	}
 	usernames := md.Get("username")
 	if len(usernames) != 1 {
+		srv.Logger.Warn("invalid request to Stream", "len", len(usernames))
 		return status.Error(codes.Unauthenticated, "Invalid username in metadata")
 	}
 	username := usernames[0]
@@ -55,22 +57,25 @@ func (srv *GrpcServer) Stream(cnn proto.Chat_StreamServer) error {
 
 	message, err := srv.Db.GetLastMessage(ctx)
 	if err != nil {
+		srv.Logger.Warn("could not get last message", "err", err.Error())
 		return status.Error(codes.Internal, "could not get last message")
 	}
 
 	err = cnn.Send(&proto.Message{MessageId: message.MessageId, Username: message.Username, Content: message.Content})
 	if err != nil {
+		srv.Logger.Info("could not send last message to", "username", username)
 		return status.Error(codes.Unavailable, "can not send last message")
 	}
 
 	for {
 		sendReq, err := cnn.Recv()
 		if err != nil {
+			srv.Logger.Info("close connection", "username", username)
 			return status.Error(codes.Aborted, "lost connection")
 		}
 		id, err := srv.Db.SaveMessage(ctx, &Message{Username: username, Content: sendReq.GetText()})
 		if err != nil {
-			//log
+			srv.Logger.Warn("could not save message", "err", err.Error())
 			continue
 		}
 		srv.Connections.SendMessageToActiveUsers(&proto.Message{MessageId: id, Username: username, Content: sendReq.GetText()})
@@ -79,20 +84,21 @@ func (srv *GrpcServer) Stream(cnn proto.Chat_StreamServer) error {
 
 func (srv *GrpcServer) LoadHistory(ctx context.Context, hisReq *proto.HistoryRequest) (*proto.HistoryResponse, error) {
 	start := int64(hisReq.LastMessageId) - int64(hisReq.Amount)
-	if start < 1 {
-		start = 1
-	}
+	start = max(start, 1)
 	if hisReq.LastMessageId < 1 {
+		srv.Logger.Warn("Invalid request at LoadHistory", "LastMessageId", hisReq.LastMessageId)
 		return nil, status.Error(codes.InvalidArgument, "last_message id should be >= 1")
 	}
 
 	messages, err := srv.Db.GetMessages(ctx, uint64(start), hisReq.LastMessageId)
 	if err != nil {
+		srv.Logger.Warn("Error in db.GetMessages", "start", start, "end", hisReq.LastMessageId)
 		return nil, status.Error(codes.Internal, "could not load history of messages")
 	}
 	var protoMessages []*proto.Message
 	for _, v := range messages {
 		protoMessages = append(protoMessages, &proto.Message{MessageId: v.MessageId, Username: v.Username, Content: v.Content})
 	}
+	srv.Logger.Info("Succesfully loaded history")
 	return &proto.HistoryResponse{Messages: protoMessages}, nil
 }
